fe942de09f84212d38ef0a4ffd70833ae841bcd7
Manjey73 <aburakhin@bk.ru>
2022-06-15 10:57:00 +0300

    Create SaveParam.cs

---
+++ DrvMercury23x/DrvMercury23x.Logic/SaveParam.cs
@@ -0,0 +1,19 @@
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Scada.Comm.Drivers.DrvMercury23x
+{
+    [Serializable]
+    public class SaveParam
+    {
+        public SaveParam()
+        {
+        }
+
+        [XmlAttribute] public string arcDt { get; set; }        // Время последнего считанного архива средних мощностей
+        [XmlAttribute] public string lastSync { get; set; }     // Дата последней синхронизации времени
+        [XmlAttribute] public string serial { get; set; }       // Серийный номер
+        [XmlAttribute] public string madeDt { get; set; }       // Дата производства
+        [XmlAttribute] public string constA { get; set; }       // Постоянная счетчика
+    }
+}
