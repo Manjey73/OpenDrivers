712600bc61eaa8f2f0d356c274f0c4417463527e
Manjey73 <aburakhin@bk.ru>
2022-06-15 10:38:36 +0300

    Create CnlPrototypeFactory.cs

---
+++ DrvDanfossECL/DrvDanfossECL.Shared/CnlPrototypeFactory.cs
@@ -0,0 +1,63 @@
+// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Devices;
+using Scada.Data.Const;
+using ScadaCommFunc;
+//using System.Collections.Generic;
+//using System.Linq;
+
+namespace Scada.Comm.Drivers.DrvDanfossECL
+{
+    /// <summary>
+    /// Creates channel prototypes for a simulator device.
+    /// <para>Создает прототипы каналов для симулятора устройства.</para>
+    /// </summary>
+    internal class CnlPrototypeFactory
+    {
+        public class ActiveChannel
+        {
+            public string Name;
+            public string Code;
+            public int CnlType;
+            public int DataType;        // Тип данных Double, Integer, ASCII, Unicode
+            public string format;       // формат данных для вывода
+        }
+
+        /// <summary>
+        /// Gets the grouped channel prototypes.
+        /// </summary>
+        public static List<CnlPrototypeGroup> GetCnlPrototypeGroups(Dictionary<string, ActiveChannel> activeChannels) // Dictionary<string, ActiveChannel> activeChannel
+        {
+            List<CnlPrototypeGroup> groups = new List<CnlPrototypeGroup>();
+
+            CnlPrototypeGroup group = new CnlPrototypeGroup();
+
+            var listCnl = activeChannels;
+
+
+            foreach (var cnlprot in listCnl)
+                {
+                    group.AddCnlPrototype(cnlprot.Value.Code, cnlprot.Value.Name).Configure(cnl =>
+                    {
+                        cnl.CnlTypeID = cnlprot.Value.CnlType;
+                        cnl.DataTypeID = cnlprot.Value.DataType;
+                        
+                        cnl.FormatCode = FormatCode.N2;
+                        if (cnlprot.Value.format == "hex") cnl.FormatCode = FormatCode.X8;
+                        else if (cnlprot.Value.format == "string") cnl.FormatCode = FormatCode.String; // Для команд, передающих строку HEX
+                    });
+                }
+                groups.Add(group);
+            return groups;
+        }
+
+        /// <summary>
+        /// Gets a flatten list of the channel prototypes.
+        /// </summary>
+        public static List<CnlPrototype> GetCnlPrototypes(Dictionary<string, ActiveChannel> dict) // Dictionary<string, ActiveChannel> dict
+        {
+            return GetCnlPrototypeGroups(dict).SelectMany(group => group.CnlPrototypes).ToList();
+        }
+    }
+}
