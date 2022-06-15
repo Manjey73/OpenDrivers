408054a794274f1c5d608aee7ce3647ac75cd0c8
Manjey73 <aburakhin@bk.ru>
2022-06-15 10:59:17 +0300

    Create Mercury23xOptions.cs

---
+++ DrvMercury23x/DrvMercury23x.Shared/Mercury23xOptions.cs
@@ -0,0 +1,47 @@
+using Scada.Config;
+using System.ComponentModel;
+
+namespace Scada.Comm.Drivers.DrvMercury23x
+{
+    internal class Mercury23xOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public Mercury23xOptions()
+            : this(new OptionList())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public Mercury23xOptions(OptionList options)
+        {
+            UserPwd = options.GetValueAsString("UserPassword");
+            AdminPwd = options.GetValueAsString("AdminPassword");
+            Level = options.GetValueAsString("Level");
+        }
+
+        [Description("Пароль 1-ого уровня"), Category("Доступ")]
+        public string UserPwd { get; set; }
+
+        [Description("Пароль 2-ого уровня"), Category("Доступ")]
+        public string AdminPwd { get; set; }
+
+        [Description("Уровень доступа"), Category("Доступ")]
+        public string Level { get; set; }
+
+        /// <summary>
+        /// Adds the options to the list.
+        /// </summary>
+        [Description("")]
+        public void AddToOptionList(OptionList options)
+        {
+            options.Clear();
+            options["UserPassword"] = UserPwd;
+            options["AdminPassword"] = AdminPwd;
+            options["Level"] = Level;
+        }
+    }
+}
