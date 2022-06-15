66181a3c4eb64bc71590e10efe512b8f2235e16f
Manjey73 <aburakhin@bk.ru>
2022-06-15 10:58:01 +0300

    Create CnlPrototypeFactory.cs

---
+++ DrvMercury23x/DrvMercury23x.Shared/CnlPrototypeFactory.cs
@@ -0,0 +1,78 @@
+using Scada.Comm.Devices;
+using Scada.Data.Const;
+
+namespace Scada.Comm.Drivers.DrvMercury23x
+{
+    /// <summary>
+    /// Creates channel prototypes for a simulator device.
+    /// <para>Создает прототипы каналов для симулятора устройства.</para>
+    /// </summary>
+    internal class CnlPrototypeFactory
+    {
+        public class ActiveChannel
+        {
+            public string GroupName;
+            public string Name;
+            public string Code;
+            public int CnlType;
+#pragma warning disable 0649
+            public int Mode;
+            public string range;
+            public byte[] setCommand;
+            public int scCnt;           // Количество принимаемы байт
+            public int datalen;         // длина блока данных в байтах
+            public string format;       // формат данных для вывода
+            public int quantity;        // идентификатор запроса группы для поиска
+            public int idxPar;          // Индекс параметра в группе тегов
+            public string CnlQuantity;  // Величина переменной
+            public string unitCode;     // Размерность переменной
+#pragma warning restore 0649
+        }
+
+        /// <summary>
+        /// Gets the grouped channel prototypes.
+        /// </summary>
+        public static List<CnlPrototypeGroup> GetCnlPrototypeGroups(Dictionary<string, ActiveChannel> activeChannels)
+        {
+            List<CnlPrototypeGroup> groups = new List<CnlPrototypeGroup>();
+
+            var menuCount =
+                from p in activeChannels.Values
+                group p by p.GroupName into g
+                select new { MenuName = g.Key, counts = g.Count() };
+
+            foreach (var menu in menuCount)
+            {
+                CnlPrototypeGroup group = new CnlPrototypeGroup();
+
+                group = new CnlPrototypeGroup(menu.MenuName);
+                var listMenu = activeChannels.Where(x => x.Value.GroupName == menu.MenuName);
+
+
+                foreach (var cnlprot in listMenu)
+                {
+                    group.AddCnlPrototype(cnlprot.Value.Code, cnlprot.Value.Name).Configure(cnl =>
+                    {
+                        cnl.UnitCode = cnlprot.Value.unitCode;
+                        cnl.QuantityCode = cnlprot.Value.CnlQuantity; // TEST
+                        cnl.CnlTypeID = cnlprot.Value.CnlType;
+                        cnl.DataTypeID = DataTypeID.Double;
+                        cnl.FormatCode = FormatCode.N2;
+                        if (cnlprot.Value.format == "hex") cnl.FormatCode = FormatCode.X8;
+                        else if (cnlprot.Value.format == "string") cnl.FormatCode = FormatCode.String; // Для команд, передающих строку HEX
+                    });
+                }
+                groups.Add(group);
+            }
+            return groups;
+        }
+
+        /// <summary>
+        /// Gets a flatten list of the channel prototypes.
+        /// </summary>
+        public static List<CnlPrototype> GetCnlPrototypes(Dictionary<string, ActiveChannel> dict) // Dictionary<string, ActiveChannel> dict
+        {
+            return GetCnlPrototypeGroups(dict).SelectMany(group => group.CnlPrototypes).ToList();
+        }
+    }
+}
