16a22678060084b66cde9d4c381b1822bcf27af5
Manjey73 <aburakhin@bk.ru>
2022-11-29 15:25:25 +0300

    Update DevTemplate.cs

--- DrvDanfossECL/DrvDanfossECL.Shared/DevTemplate.cs
+++ DrvDanfossECL/DrvDanfossECL.Shared/DevTemplate.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -10,74 +5,58 @@ namespace Scada.Comm.Drivers.DrvDanfossECL
 {
 
     [Serializable]
-    public class DevTemplate : INotifyPropertyChanged
+    public class DevTemplate // : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-                => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
         public DevTemplate()
         {
-            CmdGroups = new List<CmdGroup>();
+            Parameter = new List<Parameters>();
         }
 
         [XmlAttribute] public string Name { get; set; } // Имя устройства
+        [XmlAttribute] public string WriteEvenRAM { get; set; } // Записывать младший байт по нечетному адресу RAM
+        [XmlAttribute] public string WriteOnlyRAM { get; set; } // Записы только в RAM
 
-        public List<CmdGroup> CmdGroups { get; set; }
+        [XmlIgnore]
+        public bool WriteEvenRAMSpecified { get { return WriteEvenRAM != ""; } }
+        public bool WriteOnlyRAMSpecified { get { return WriteOnlyRAM != ""; } }
 
-        public class CmdGroup
+
+        [XmlElement] public List<Parameters> Parameter { get; set; }
+                public class Parameters
         {
-            public CmdGroup()
+            public Parameters()
             {
             }
 
-            public CmdGroup(string Name, bool Active, string Code, string Read, string Write, string Parameter, string Format, string Mul, string Div) // string Format
+            public Parameters(string Code, string Name, string Address, bool Active, bool Write, string min_val, string max_val, string Format, string Multiplier) // string Format
             {
-                this.Name = Name;
-                this.Active = Active;
                 this.Code = Code;
-                this.Read = Read;
+                this.Name = Name;
+                this.Address = Address;
+                this.Active = Active;
                 this.Write = Write;
-                this.Parameter = Parameter;
+                this.min_val = min_val;
+                this.max_val = max_val;
                 this.Format = Format;
-                this.Mul = Mul;
-                this.Div = Div;
+                this.Multiplier = Multiplier;
             }
 
-            [XmlAttribute] public string Name { get; set; }         // Имя параметра
-            [XmlAttribute] public bool Active { get; set; }         // Активность параметра
             [XmlAttribute] public string Code { get; set; }         // Код параметра
-            [XmlAttribute] public string Read { get; set; }         // Байт для чтения параметра
-            [XmlAttribute] public string Write { get; set; }        // Байт для записи параметра
-            [XmlAttribute] public string Parameter { get; set; }    // Байт параметра
-            [XmlAttribute] public string Format { get ; set; }       // Формат переменной (float, int, string и т.д.) // public string Format { get; set; }
-            [XmlAttribute] public string Mul { get; set; }          // Множитель параметра
-            [XmlAttribute] public string Div { get; set; }          // Делитель параметра
+            [XmlAttribute] public string Name { get; set; }         // Имя параметра
+            [XmlAttribute] public string Address { get; set; }         // Имя параметра
+            [XmlAttribute] public bool Active { get; set; }         // Активность параметра
+            [XmlAttribute] public bool Write { get; set; }       // Разрешение записи
+            [XmlAttribute] public string min_val { get; set; }      // Минимальное значение
+            [XmlAttribute] public string max_val { get; set; }      // Максимальное значение
+            [XmlAttribute] public string Format { get; set; }       // Формат переменной (float, int, string и т.д.)
+            [XmlAttribute] public string Multiplier { get; set; }   // Множитель параметра
 
-            public bool MulSpecified { get { return Mul != ""; } }
-            public bool DivSpecified { get { return Div != ""; } }
+            [XmlIgnore]
+            public bool min_valSpecified { get { return min_val != ""; } }
+            public bool max_valSpecified { get { return max_val != ""; } }
+            public bool MultiplierSpecified { get { return Multiplier != ""; } }
+
         }
 
-        //public class Choice
-        //{
-        //    public string Value { get; private set; }
-        //    public Choice(string value) // string name, int value
-        //    {
-        //        Value = value;
-        //    }
-
-        //    private static readonly List<Choice> possibleChoices = new List<Choice>
-        //    {
-        //        { new Choice(null) },
-        //        { new Choice("float") },
-        //        { new Choice("int16") }
-        //    };
-
-        //    public static List<Choice> GetChoices()
-        //    {
-        //        return possibleChoices;
-        //    }
-        //}
     }
 }
