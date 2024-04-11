c59124dff0085895bd271621ad040eac63148ca2
Manjey73 <aburakhin@bk.ru>
2024-04-11 09:29:53 +0300

    Create DriverUtils.cs

---
+++ DrvDS18B20/DrvDS18B20.Shared/DriverUtils.cs
@@ -0,0 +1,14 @@
+namespace Scada.Comm.Drivers.DrvDS18B20
+{
+    /// <summary>
+    /// The class provides helper methods for the driver.
+    /// <para>Класс, предоставляющий вспомогательные методы для драйвера.</para>
+    /// </summary>
+    public static class DriverUtils
+    {
+        /// <summary>
+        /// The driver code.
+        /// </summary>
+        public const string DriverCode = "DrvDS18B20";
+    }
+}
